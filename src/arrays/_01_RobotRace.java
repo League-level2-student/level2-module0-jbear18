6181da864d50424ff69d6d0311f4abb7fc058a86
munchman <nootnoot@gmail.com>
2018-10-12 16:58:54 -0700

    Robot Race IP

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,12 +1,20 @@
 package arrays;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
+public static void main(String[] args) {
+	
 
 	//2. create an array of 5 robots.
-
+Robot [] mikes= new Robot[5];
 	//3. use a for loop to initialize the robots.
-
+for (int i = 0; i < mikes.length; i++) {
+	mikes[i]= new Robot("batman");
+	mikes[i].setX(i*100+200);
+	mikes[i].setY(729);
+}
 		//4. make each robot start at the bottom of the screen, side by side, facing up
 
 	//5. use another for loop to iterate through the array and make each robot move 
@@ -19,4 +27,6 @@ public class _01_RobotRace {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+	
+}
 }
